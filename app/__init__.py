cbd66f69929a9a7e66c62f6fb0e479a0d0bc56ce
Mokomcalixte <calixtemokom@gmail.com>
2024-10-13 19:18:38 +0200

    new commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_cors import CORS  # Importer CORS
 from app.routes import bp as book_routes
 
 def create_app():
     app = Flask(__name__)
+    
+    # Activer CORS pour l'application
+    CORS(app)  # Ajoute cette ligne pour permettre les requêtes cross-origin
+
     app.register_blueprint(book_routes)
     return app
